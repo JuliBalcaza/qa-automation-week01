4e0e80dc4f2814360cbe6efab0799b73e13f6ec4
Julieta Balcaza <julieta.balcaza@radiumrocket.com>
2023-06-26 21:21:13 -0300

    QA-W01: add user name for e2e apiCommands

--- test/specs/user.e2e.ts
+++ test/specs/user.e2e.ts
@@ -3,6 +3,7 @@ import { createUser, getUserData, deleteUser } from "../utils/apiCommands.js";
 describe('API commands', () => {
     let userId: number;
     let userEmail: string;
+    let userName: string;
 
     afterAll(async () => {
         await deleteUser(userId);
@@ -22,8 +23,11 @@ describe('API commands', () => {
     it('Should GET command', async () => {
         const userData =  await getUserData(userId);
         userEmail = userData.email;
+        userName = userData.name;
 
         console.log("The user email is: ", await userEmail);
+        console.log("The user name is: ", await userName);
+        
         
     });
 
